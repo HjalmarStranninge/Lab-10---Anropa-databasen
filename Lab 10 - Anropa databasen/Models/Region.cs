e4170059c719feacb15d243d2296ae8527f39e92
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2023-11-01 09:37:32 +0100

    Kopplat mot northwind databasen

---
+++ Lab 10 - Anropa databasen/Models/Region.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_10___Anropa_databasen.Models;
+
+public partial class Region
+{
+    public int RegionId { get; set; }
+
+    public string RegionDescription { get; set; } = null!;
+
+    public virtual ICollection<Territory> Territories { get; set; } = new List<Territory>();
+}
---
+++ Lab 10 - Anropa databasen/Models/SummaryOfSalesByYear.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_10___Anropa_databasen.Models;
+
+public partial class SummaryOfSalesByYear
+{
+    public DateTime? ShippedDate { get; set; }
+
+    public int OrderId { get; set; }
+
+    public decimal? Subtotal { get; set; }
+}
